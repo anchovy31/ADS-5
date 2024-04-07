419d486222d643ebcf4818ae170e0574323a235a
anchovy31 <146199120+anchovy31@users.noreply.github.com>
2024-04-07 11:04:50 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,22 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  T data[size];
+  int top;
+public:
+  Tstack() {
+    top = 0;
+  }
+  void Push(const T& item) {
+    if (top <= size - 1)
+      data[top++] = item;
+    else
+      throw string("Full!");
+  }
+  T Pop() {
+    if (top > 0)
+      return data[--top];
+    else throw string("Empty!");
+  }
 };
-
 #endif  // INCLUDE_TSTACK_H_
